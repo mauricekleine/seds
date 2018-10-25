60199cea551fda5d11115d77391707afcd854e93
Maurice Kleine <info@mauricekleine.com>
2018-10-25 17:08:17 +0530

    responsive styles

--- components/Container.js
+++ components/Container.js
@@ -5,7 +5,29 @@ export default ({ children, ...props }) => (
     <style jsx>{`
       div {
         margin: 16px auto;
-        width: 66%;
+        width: 100%;
+      }
+
+      // Bootstrap container styles
+      @media (min-width: 1200px) {
+        div {
+          max-width: 1140px;
+        }
+      }
+      @media (min-width: 992px) {
+        div {
+          max-width: 960px;
+        }
+      }
+      @media (min-width: 768px) {
+        div {
+          max-width: 720px;
+        }
+      }
+      @media (min-width: 576px) {
+        div {
+          max-width: 540px;
+        }
       }
     `}</style>
   </div>
