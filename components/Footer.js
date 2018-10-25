213dd44a4ca9b76eff4f55ab624a8ab8d8e996a2
Maurice Kleine <info@mauricekleine.com>
2018-10-25 17:02:15 +0530

    dynamic copyright year

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,6 @@ import Container from "./Container";
 
 export default () => (
   <Container>
-    Copyright 2018 - All information on this site is part of SEDS
+    Copyright {new Date().getFullYear()} - All information on this site is part of SEDS
   </Container>
 );
