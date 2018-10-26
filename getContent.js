28fc3a70dd3c2eb902b6cf22d73a074c49fd08ba
Maurice Kleine <info@mauricekleine.com>
2018-10-26 11:23:43 +0530

    fetch contentful data

---
+++ getContent.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config({
+  path: ".env"
+});
+
+const client = require("contentful").createClient({
+  space: process.env.CONTENTFUL_SPACE,
+  accessToken: process.env.CONTENTFUL_TOKEN
+});
+
+const filePath = path.join(__dirname, "data");
+
+if (!fs.existsSync(filePath)) {
+  fs.mkdirSync(filePath);
+}
+
+["reports"].map(async type => {
+  const entries = await client.getEntries({
+    content_type: type
+  });
+
+  if (entries.total === 1) {
+    const { fields } = entries.items[0];
+
+    fs.writeFileSync(
+      path.join(filePath, `${type}.json`),
+      JSON.stringify(fields)
+    );
+  }
+});
